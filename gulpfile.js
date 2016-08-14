ffffd69ffa8b44450bda3f72eed1e79accd9b9b2
Denny Tek <tek.denny@gmail.com>
2016-08-14 12:20:05 -0400

    Add custom date filter

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,24 @@ gulp.task('browserSync', function () {
 // set up nunjucks environment
 function nunjucksEnv(env) {
   env.addFilter('slug', slugify);
+  env.addFilter('dateFilter', dateFilter);
   // env.addFilter('split', split);
+}
+
+function dateFilter(date, arg) {
+  var locale = "en-us";
+  var date = new Date(date);
+  switch (arg) {
+    case "month":
+      return date.toLocaleString(locale, { month: "long" });
+      break;
+    case "day":
+      return date.getDate();
+      break;
+    case "year":
+      return date.getFullYear();
+      break;
+  }
 }
 
 // converts string t to a slug (eg 'Some Text Here' becomes 'some-text-here')
